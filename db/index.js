0c8b044f411c46daac6fdfa306e0b3746fc7f3a5
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-12-01 16:57:35 -0600

    finished index.js in db directory

--- db/index.js
+++ db/index.js
@@ -11,5 +11,70 @@ class DB {
         );
     }
 
+    findAllPossibleManagers(employeeId) {
+        return this.connection.promise().query(
+          "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+          employeeId
+        );
+      }
     
-}
+      createEmployee(employee) {
+        return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+      }
+    
+      removeEmployee(employeeId) {
+        return this.connection.promise().query(
+          "DELETE FROM employee WHERE id = ?",
+          employeeId
+        );
+      }
+    
+      updateEmployeeRole(employeeId, roleId) {
+        return this.connection.promise().query(
+          "UPDATE employee SET role_id = ? WHERE id = ?",
+          [roleId, employeeId]
+        );
+      }
+      
+    
+      findAllRoles() {
+        return this.connection.promise().query(
+          "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+        );
+      }
+    
+      createRole(role) {
+        return this.connection.promise().query("INSERT INTO role SET ?", role);
+      }
+    
+      removeRole(roleId) {
+        return this.connection.promise().query("DELETE FROM role WHERE id = ?", roleId);
+      }
+    
+      findAllDepartments() {
+        return this.connection.promise().query(
+          "SELECT department.id, department.name FROM department;"
+        );
+        }
+    
+      createDepartment(department) {
+        return this.connection.promise().query("INSERT INTO department SET ?", department);
+      }
+    
+      removeDepartment(departmentId) {
+        return this.connection.promise().query(
+          "DELETE FROM department WHERE id = ?",
+          departmentId
+        );
+      }
+    
+     
+      findAllEmployeesByDepartment(departmentId) {
+        return this.connection.promise().query(
+          "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
+          departmentId
+        );
+      }
+    }
+    
+module.exports = new DB(connection);
