576a24caacf3d1121daf9a7dfa923a4cb3ca7925
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-12-01 10:04:10 -0600

    added role and values for role into seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,15 @@ VALUES
     ('Engineering'),
     ('Finance'),
     ('Legal'),
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 75000, 1),
+    ('Salesperson', 60000, 1),
+    ('Lead Engineer', 120000, 2),
+    ('Mechanical Engineer', 105000, 2),
+    ('Software Engineer', 110000, 2),
+    ('Financial Manager', 135000, 3),
+    ('Accountant', 115000, 3),
+    ('Lead Lawyer', 200000, 4),
+    ('Lawyer', 175000, 4),
