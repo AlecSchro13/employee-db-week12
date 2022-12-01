f17f307220b260b4b21fca2abdfde316d665695e
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-12-01 10:33:09 -0600

    started in index.js, started main prompt function

--- index.js
+++ index.js
@@ -2,4 +2,68 @@ const { prompt } = require("inquirer");
 const db = require("./db");
 require("console.table");
 
-init()
+init();
+
+function init() {
+    loadMainPrompt();
+}
+
+function loadMainPrompt() {
+    prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "Please choose an option",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES"
+                  },
+                  {
+                    name: "View All Employees By Department",
+                    value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+                  },
+                  {
+                    name: "Add Employee",
+                    value: "ADD_EMPLOYEE"
+                  },
+                  {
+                    name: "Remove Employee",
+                    value: "REMOVE_EMPLOYEE"
+                  },
+                  {
+                    name: "Update Employee Role",
+                    value: "UPDATE_EMPLOYEE_ROLE"
+                  },
+                  {
+                    name: "View All Roles",
+                    value: "VIEW_ROLES"
+                  },
+                  {
+                    name: "Add Role",
+                    value: "ADD_ROLE"
+                  },
+                  {
+                    name: "Remove Role",
+                    value: "REMOVE_ROLE"
+                  },
+                  {
+                    name: "View All Departments",
+                    value: "VIEW_DEPARTMENTS"
+                  },
+                  {
+                    name: "Add Department",
+                    value: "ADD_DEPARTMENT"
+                  },
+                  {
+                    name: "Remove Department",
+                    value: "REMOVE_DEPARTMENT"
+                  },
+                  {
+                    name: "Quit",
+                    value: "QUIT"
+                  }
+            ]
+        }
+    ])
+}
